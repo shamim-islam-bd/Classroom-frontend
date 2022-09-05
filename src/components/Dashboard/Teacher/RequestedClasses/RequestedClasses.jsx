6e25c2583ee6e7ac4ff64cf1d883f834d287ce48
Shamimzz <shamimislam5091@gmail.com>
2022-09-05 20:12:24 +0600

    added

--- src/components/Dashboard/Teacher/RequestedClasses/RequestedClasses.jsx
+++ src/components/Dashboard/Teacher/RequestedClasses/RequestedClasses.jsx
@@ -1,20 +1,24 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { getAllStudentClassRequest } from "../../../../Store/Actions/StudentClassReqAction";
 
 export default function RequestedClasses() {
-  const navigate = useNavigate();
-  const  dispatch = useDispatch();
+  //   const navigate = useNavigate();
+  const dispatch = useDispatch();
   useSelector((state) => console.log("From RqstClass Component: ", state));
+//   const 
 
-
-//   axios.get('')
-
-
-  useEffect(() => {
-     dispatch(getAllStudentClassRequest())
-  }, []);
+  // geting requested classes data from redux store useing axios
+  axios
+    .get("/students-Class-Request")
+    .then((res) => {
+      console.log("students-Class-Request: ", res.data);
+      // dispatch(res.data);
+      // setStudentClassRequest(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
   return <div className="mt-10">RequestedClasses</div>;
 }
