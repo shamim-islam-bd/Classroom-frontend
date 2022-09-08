31b3085632f14e39d872d0fc5cc75646bac253a2
Shamimzz <shamimislam5091@gmail.com>
2022-09-08 10:07:51 +0600

    added

--- src/components/Dashboard/Teacher/ClassScheduling/ClassScheduling.jsx
+++ src/components/Dashboard/Teacher/ClassScheduling/ClassScheduling.jsx
@@ -1,9 +1,13 @@
+import "@fullcalendar/core/vdom"; // solves problem with Vite
+import dayGridPlugin from "@fullcalendar/daygrid";
 import FullCalendar from "@fullcalendar/react";
-import { Col, Layout, Row } from "antd";
+
 import React from "react";
+
+import { Col, Layout, Row } from "antd";
 import { events } from "./data";
 
-import dayGridPlugin from "@fullcalendar/daygrid";
+// import dayGridPlugin from "@fullcalendar/daygrid";
 import listPlugin from "@fullcalendar/list";
 
 export default function ClassScheduling() {
@@ -17,6 +21,11 @@ export default function ClassScheduling() {
           <Col className="p-3" span={24} sm={24} xm={24} md={24} lg={14}>
             <FullCalendar
               plugins={[dayGridPlugin, listPlugin]}
+              headerToolbar={{
+                left: "today prev,next prevYear,nextYear",
+                center: "title",
+                right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+              }}
               initialView="listWeek"
               events={events}
             />
